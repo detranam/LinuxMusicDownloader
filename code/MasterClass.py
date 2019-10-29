9a87f607ed95866593531154e7bfe1eabd29c4d4
Detrana <detranam@ad.msoe.edu>
2019-10-29 13:38:38 -0500

    Changes file structure, starts multithreading

---
+++ code/MasterClass.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+"""Multithreads a music downloader
+
+This allows for multithreading the music downloading script 'b1.sh'.
+b1.sh takes in a music list and uses youtube-dl to download the music
+(in mp3 file format) to the current directory
+"""
+import classes
+
+__author__ = "Matthew DeTrana"
+__license__ = "Apache 2.0"
+__credits__ = ["Matthew DeTrana"]
+__version__ = "1.0.1"
+__maintainer__ = "Matthew DeTrana"
+__email__ = "detranam.code@gmail.com"
+__status__ = "Development"
