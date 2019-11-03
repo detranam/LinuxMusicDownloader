db5234b93e440bc9d2378bd4e8759a3d00e7d446
Detrana <detranam@ad.msoe.edu>
2019-11-03 11:02:11 -0600

    adds Apache 2.0 copyright

--- code/MasterClass.py
+++ code/MasterClass.py
@@ -5,6 +5,20 @@
 This allows for multithreading the music downloading script 'b1.sh'.
 b1.sh takes in a music list and uses youtube-dl to download the music
 (in mp3 file format) to the current directory
+
+Copyright 2019 Matthew DeTrana
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
 """
 import classes
 
