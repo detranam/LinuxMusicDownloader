0add72c97f77bf4a894d7f35effe3563dda60e72
detranam <detranam.code@gmail.com>
2021-05-12 09:56:55 -0500

    Renamed a file, added an additional class to help

---
+++ downloader.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""Multithreads a music downloader
+
+This allows for multithreading the music downloading script 'b1.sh'.
+b1.sh takes in a music list and uses youtube-dl to download the music
+(in mp3 file format) to the current directory
+
+
+LinuxMusicDownloader
+Copyright © 2021 Matthew DeTrana
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.
+"""
+
+from tinytag import TinyTagException
+import musicman
+import argparse
+
+
+__author__ = "Matthew DeTrana"
+__license__ = "MIT"
+__version__ = "1.0.1"
+__status__ = "Development"
+
+# python -m pip install git+https://github.com/blackjack4494/yt-dlc
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--move", help="Whether or not to move any downloaded songs to a 'songs_out' folder")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-t", "--text", help="Whether or not to download from a provided text file line-by-line")
+group.add_argument("-p", "--playlist", action="store_true", help="Whether or not to download from the stored playlists")
+args = parser.parse_args()
+try:
+    if args.text:
+        musicman.download_txt_songs(args.text)
+    elif args.playlist:
+        musicman.download_playlist_songs()
+except TinyTagException as err:
+        print("ERROR: TinyTag threw an error: " + err)
--- musicman.py
+++ musicman.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-
-"""Multithreads a music downloader
-
-This allows for multithreading the music downloading script 'b1.sh'.
-b1.sh takes in a music list and uses youtube-dl to download the music
-(in mp3 file format) to the current directory
-
-
-LinuxMusicDownloader
-Copyright © 2021 Matthew DeTrana
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
-to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-IN THE SOFTWARE.
-"""
-
 from sys import argv
 from pathlib import Path
 from tinytag import TinyTag, TinyTagException
@@ -29,15 +5,8 @@ from subprocess import run
 import os
 import shutil
 
-__author__ = "Matthew DeTrana"
-__license__ = "MIT"
-__version__ = "1.0.1"
-__email__ = "detranam.code@gmail.com"
-__status__ = "Development"
-
-# python -m pip install git+https://github.com/blackjack4494/yt-dlc
-
 music_dict = {}
+
 # Populate this 'youtube playlist links' list to whatever
 # playlists you want to download all songs from.
 youtube_playlist_links = [
@@ -46,6 +15,8 @@ youtube_playlist_links = [
 # This is the location of which to move files downloaded into the current
 # directory
 final_destination = "songs_out"
+if __name__ == "__main__":
+    print("ERROR: Cannot run musicman alone!")
 
 
 def populate_music_dict(search_path):
@@ -92,7 +63,7 @@ def download_playlist_songs():
     move_mp3_to_output()
 
 
-def download_txt_songs():
+def download_txt_songs(filename):
     # this will eventually be passing in a *.txt file and searching for
     # each line-item song
     pass
@@ -101,13 +72,3 @@ def download_txt_songs():
          "mp3", "--min-views", "1500", "--geo-bypass", "-i",
                 "--embed-thumbnail"])
     move_mp3_to_output()
-
-
-if __name__ == "__main__":
-    try:
-        download_playlist_songs()
-        # move_mp3_to_output()
-        # download_new_songs()
-        # populate_music_dict(argv[1])
-    except TinyTagException as err:
-        print("ERROR: TinyTag threw an error: " + err)
